25c8bd14242dbbdca0fa1f7b9e5d2386818e130b
Dâmaso Júnio Pereira Brasileo <Junio.pereirab@gmail.com>
2021-10-14 00:13:22 -0300

    Refatorando os testes para um código mais limpo.

--- tests/FileHandlerTests.java
+++ tests/FileHandlerTests.java
@@ -1,21 +1,50 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class FileHandlerTests {
 
+    @Parameters
+    public static Iterable getParameters() {
+        Object[][] data = new Object[][] {
+                {";", ';'},
+                {"\r", '\r'},
+                {"\n", '\n'},
+                {"\t", '\t'}
+        };
+        return Arrays.asList(data);
+    }
+
+    private FileHandler handler;
+    private String delimiter;
+    private char expected;
+
+    public FileHandlerTests(String delimiter, char expected) {
+        this.delimiter = delimiter;
+        this.expected = expected;
+    }
+
+    @Before
+    public void initialize() {
+        handler = new FileHandler();
+    }
+
     @Test
-    public void receiveDelimiterChar() {
-        String delimiter = ";";
-        FileHandler handler = new FileHandler();
+    public void receiveDelimiterCharWithSuccess() {
         handler.setDelimiter(delimiter);
-        assertEquals(';', handler.getDelimiter());
+        assertEquals(expected, handler.getDelimiter());
     }
 
     @Test(expected = DelimitadorInvalidoException.class)
-    public void receiveDelimiterString() {
+    public void receiveDelimiterStringWithFailure() {
         String delimiter = "test";
-        FileHandler handler = new FileHandler();
         handler.setDelimiter(delimiter);
     }
 }
