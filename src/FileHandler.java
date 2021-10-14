fb76c6e426bdcaafd2172f311923098b398b2af2
Dâmaso Júnio Pereira Brasileo <Junio.pereirab@gmail.com>
2021-10-13 23:51:49 -0300

    #2 - Teste de exceção passando

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -3,6 +3,9 @@ public class FileHandler {
     private String delimiter;
 
     public void setDelimiter(String delimiter) {
+        if(delimiter.length() != 1){
+            throw new DelimitadorInvalidoException("Delimitador deve ser apenas um caracter");
+        }
         this.delimiter = delimiter;
     }
 
