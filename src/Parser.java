db51aa9b714ead1d43603956354a35c68760abd7
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2021-10-16 23:40:38 -0300

    #4 - Finalizando a logica para a escolha da orientacao da saida da aplicacao.

--- src/Parser.java
+++ src/Parser.java
@@ -15,12 +15,42 @@ public class Parser {
         this.orientation = orientation;
     }
 
+    private String transpose(List<List<String>> convolutions, int max_index) {
+        String converted = new String("");
+
+        for (int i = 0; i < max_index; i++) {
+            for (int j = 0; j < convolutions.size(); j++) {
+                try {
+                    converted = converted.concat(convolutions.get(j).get(i) + this.delimitator);
+                } catch(Exception excpt){
+                    converted = converted.concat(String.valueOf(this.delimitator));
+                }
+            }
+
+            if (i != max_index - 1)
+                converted = converted.concat("\n");
+        }
+        
+        return converted;
+    }
+
+    private int getMaxIndex(List<List<String>> convolutions) {
+        int max_index = -1000;
+
+        for (int i = 0; i < convolutions.size(); i++) {
+            if (convolutions.get(i).size() > max_index)
+                max_index = convolutions.get(i).size();
+        }
+
+        return max_index;
+    }
+
     private String parseHorizontally(String result) {
         int dump = 0;
         int counter = 1;
 
         for (int i = 0; i < this.content.size(); i++) {
-            if (this.content.get(i).contains("-----") && i != 0) {
+            if (i != 0 && this.content.get(i).contains("-----")) {
                 result = result.concat("\n");
                 this.analises.add(dump);
                 dump = 0;
@@ -39,5 +69,56 @@ public class Parser {
         this.analises.add(dump);
         this.iteration = counter;
         return result;
+    }
+
+    private String parseVertically(String tgt) {
+        String[] split_str = tgt.split(String.valueOf(this.delimitator));
+        List<List<String>> convolutions = new ArrayList<List<String>>();
+        List<String> convo_dump = new ArrayList<String>();
+
+        String col = new String("");
+        String res = new String("");
+
+        for (int i = 0; i < split_str.length; i++) {
+            if (i == split_str.length - 1)
+                convolutions.add(convo_dump);
+
+            if (i == 0)
+                col = col.concat(split_str[i] + this.delimitator);
+
+            else if (split_str[i].contains("\n")) {
+                col = col.concat(split_str[i].replace("\n", "") + this.delimitator);
+                convolutions.add(convo_dump);
+                convo_dump = new ArrayList<String>();
+            } else {
+                convo_dump.add(split_str[i]);
+            }
+        }
+
+        int max_index = getMaxIndex(convolutions);
+        String converted = transpose(convolutions, max_index);
+        col = col.concat("\n");
+        res = res.concat(col);
+        res = res.concat(converted);
+        return res;
+    }
+
+    public String removeInvalidChars(String tgt) {
+        tgt = tgt.replaceAll(this.delimitator + "$", "");
+        tgt = tgt.replaceAll(this.delimitator + "\n", "\n");
+
+        return tgt;
+    }
+
+    public String getParsedData() {
+        String res = new String("");
+        res = parseHorizontally(res);
+
+        if (this.orientation == 'v') {
+            String vertical_parsed = parseVertically(res);
+            return removeInvalidChars(vertical_parsed);
+        }
+
+        return removeInvalidChars(res);
     }
 }
